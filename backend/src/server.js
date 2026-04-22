a61b9665c495d1abbaed58ef98ba4eb7baf80e42
Nuno Esteves <nuno.esteves89@gmail.com>
2026-04-22 19:19:26 +0200

    fix: Mise à jours du projet

--- backend/src/server.js
+++ backend/src/server.js
@@ -82,10 +82,10 @@ app.use((req, res, next) => {
     'Content-Security-Policy',
     "default-src 'self'; " +
     "script-src 'self'; " +
-    "style-src 'self' 'unsafe-inline'; " +
+    "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; " +
     "img-src 'self' data: https:; " +
-    "font-src 'self'; " +
-    "connect-src 'self'; " +
+    "font-src 'self' https://fonts.gstatic.com; " +
+    "connect-src 'self' https://portfolio-api-uj5s.onrender.com; " +
     "frame-ancestors 'none'; " +
     "base-uri 'self'; " +
     "form-action 'self';"
